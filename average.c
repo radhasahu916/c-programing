7aeeb66266d216d82582f8a4615bf04cfeff8ee8
radhasahu916 <radhasahu5657@gmail.com>
2025-09-12 13:21:07 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+int a;
+int b;
+int c;
+int d;
+
+  printf ("enter your first value ");
+  scanf ("%d",&a);
+
+printf ("inter your second value");
+  scanf  ("%d",&b);
+
+  printf ("inter your third value");
+  scanf ("%d",&c);
+
+  d= (a+b+c) /3;
+  printf ("% float",d);
+}
+
