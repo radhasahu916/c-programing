3b6edf659218ef75395455ce9a4f5b8b8352503d
radhasahu916 <radhasahu5657@gmail.com>
2025-11-20 12:40:16 +0530

    Add files via upload

---
+++ assignment3.4.2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main(){
+ int number;
+
+printf("enter number");
+scanf("%d",&number);
+
+if(number==0){
+   printf("zero");
+}
+
+else if(number==1){
+    printf("one");
+}
+else if(number==2){
+    printf("two");
+}
+else if(number==3){
+    printf("three");
+}
+else if(number==4){
+    printf("four");
+}
+else if(number==5){
+    printf("five");
+}
+else if(number==6){
+    printf("six");
+}
+else if(number==7){
+    printf("seven");
+}
+else if(number==8){
+printf("eight");
+}
+else if(number==9){
+    printf("nine");
+}
+else{
+    printf("invalide number");
+}
+
+return 0;
+
+}
